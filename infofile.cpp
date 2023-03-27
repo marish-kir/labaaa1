870c9a19ef79eb6933698c02ac8f923a9ee0b606
Marina Kiryachek <radkovaaa@yandex.ru>
2023-03-27 23:44:43 +0700

    добавлен конструктор с параметром

--- infofile.cpp
+++ infofile.cpp
@@ -6,7 +6,12 @@ InfoFile::InfoFile()
 
 
 }
-
+InfoFile::InfoFile(QString _name)
+{
+    QFileInfo info(_name);
+    size = info.size();
+    state = info.exists();
+}
 int InfoFile::get_size()
 {
     return size;
--- infofile.h
+++ infofile.h
@@ -1,11 +1,28 @@
 #ifndef INFOFILE_H
 #define INFOFILE_H
+#include <QString>
+#include <QFileInfo>
 
 
 class InfoFile
 {
+   private:
+    int size;
+    QString name;
+    bool state;
+
 public:
     InfoFile();
+    InfoFile(QString _name);
+
+
+    int get_size();
+    QString get_name();
+    bool get_state();
+
+
+
+
 };
 
 #endif // INFOFILE_H
