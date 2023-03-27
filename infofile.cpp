c06ce3212018273de8bfe2f2b6b1f057fc426739
Marina Kiryachek <radkovaaa@yandex.ru>
2023-03-27 23:26:25 +0700

    добавлена реализация класса infofile

--- infofile.cpp
+++ infofile.cpp
@@ -1,6 +1,21 @@
 #include "infofile.h"
 
+
 InfoFile::InfoFile()
 {
 
+
+}
+
+int InfoFile::get_size()
+{
+    return size;
+}
+QString InfoFile::get_name()
+{
+    return name;
+}
+bool InfoFile::get_state()
+{
+    return state;
 }
